005442d670c84fad67b59e5ba318338f6dc8bea1
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-01-31 17:43:05 -0800

    moved to edge 1.0.6

--- test-runner/edge_configuration.py
+++ test-runner/edge_configuration.py
@@ -32,12 +32,12 @@ class EdgeConfiguration:
         if False and len(os.environ.get("IOTHUB_E2E_EDGE_PRIVATE_AGENTIMAGE", None) or "") > 0:
             self.agentImage = os.environ["IOTHUB_E2E_EDGE_PRIVATE_AGENTIMAGE"]
         else:
-            self.agentImage = "mcr.microsoft.com/azureiotedge-agent:1.0.5"
+            self.agentImage = "mcr.microsoft.com/azureiotedge-agent:1.0.6"
 
         if False and len(os.environ.get("IOTHUB_E2E_EDGE_PRIVATE_HUBIMAGE", None) or "") > 0:
             self.hubImage = os.environ["IOTHUB_E2E_EDGE_PRIVATE_HUBIMAGE"]
         else:
-            self.hubImage = "mcr.microsoft.com/azureiotedge-hub:1.0.5"
+            self.hubImage = "mcr.microsoft.com/azureiotedge-hub:1.0.6"
 
         self.config = {
             "moduleContent": {
