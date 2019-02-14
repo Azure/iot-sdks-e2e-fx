5e18893665ccb2cae61df6fcc6e96ae590cbd2ce
Bert Kleewein <bertk@microsoft.com>
2019-02-14 00:05:13 -0800

    better logging for ensure-container

--- pyscripts/ensure-container.py
+++ pyscripts/ensure-container.py
@@ -5,19 +5,29 @@ import argparse
 import requests
 import sys
 import time
+import json
+import inspect
+from pprint import pprint
 
 containerMap = {
-  'nodeMod':  8080,
-  'pythonMod': 8081,
-  'cMod': 8082,
-  'csharpMod': 8083,
-  'javaMod': 8084,
-  'pythonpreviewMod': 8085,
-  'friendMod': 8099,
+    "nodeMod": 8080,
+    "pythonMod": 8081,
+    "cMod": 8082,
+    "csharpMod": 8083,
+    "javaMod": 8084,
+    "pythonpreviewMod": 8085,
+    "friendMod": 8099,
 }
 
-parser = argparse.ArgumentParser(description="ensure that a test container is running.  Restart the container if it does not respond.")
-parser.add_argument("container_name", help="container to validate", type=str, choices=containerMap.keys())
+parser = argparse.ArgumentParser(
+    description="ensure that a test container is running.  Restart the container if it does not respond."
+)
+parser.add_argument(
+    "container_name",
+    help="container to validate",
+    type=str,
+    choices=containerMap.keys(),
+)
 args = parser.parse_args()
 
 container_name = args.container_name
@@ -25,35 +35,36 @@ port = containerMap[container_name]
 client = docker.from_env()
 
 try:
-  container = client.containers.get(container_name)
+    container = client.containers.get(container_name)
 except docker.errors.NotFound:
-  print("Container {} is not deployed".format(container_name))
-  sys.exit(1)
+    print("Container {} is not deployed".format(container_name))
+    sys.exit(1)
 
 restart_attempts = 10
 container_startup_time = 5
 
 for f in range(0, restart_attempts):
-  try:
-    r = requests.put("http://localhost:{}/wrapper/message".format(port), json={"msg": "test message from ensure_container.py"})
-  except Exception as e:
-    print("Container {} is not responding".format(container_name))
-    print(str(e))
-  else:
-    print("Container {} is running and responding".format(container_name))
-    sys.exit(0)
+    try:
+        uri = "http://localhost:{}/wrapper/message".format(port)
+        body = {"msg": "test message {} from ensure_container.py".format(f)}
+        print("Sending {} to {}".format(json.dumps(body), uri))
+        r = requests.put(uri, json=body)
+    except Exception as e:
+        print("Container {} is not responding".format(container_name))
+        print(str(e))
+    else:
+        print("Container {} is running and responding".format(container_name))
+        pprint(r.__dict__)
+        sys.exit(0)
 
-  print("restarting container {}".format(container_name))
-  container.restart()
+    print("restarting container {}".format(container_name))
+    container.restart()
 
-  time.sleep(container_startup_time)
+    time.sleep(container_startup_time)
 
-print("Container {} did not respond after {} restart attempts".format(container_name, restart_attempts))
+print(
+    "Container {} did not respond after {} restart attempts".format(
+        container_name, restart_attempts
+    )
+)
 sys.exit(1)
-
-
-
-
-
-
-
