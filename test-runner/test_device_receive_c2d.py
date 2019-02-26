eb0dc83a38d05af77ae75b7787660818a1330e6e
olivakar <oliva.kar@microsoft.com>
2019-02-25 18:40:34 -0800

    working c2d

--- test-runner/test_device_receive_c2d.py
+++ test-runner/test_device_receive_c2d.py
@@ -9,9 +9,8 @@ import connections
 import test_utilities
 import environment
 from adapters import print_message as log_message
-import concurrent
-import asyncio
 import time
+import threading
 
 
 @pytest.mark.testgroup_iothub_module_client
@@ -23,17 +22,19 @@ def test_device_receive_c2d_from_iothub():
 
     log_message("connecting device client")
     device_client = connections.connect_leaf_device_client()
-
     expected_message = "Mimbulus Mimbletonia"
+
+    log_message("wait for event to arrive at device client ")
+    c2d_message_queue = device_client.receive_c2d()
+
     service = connections.connect_service_client()
     service.send_c2d(environment.leaf_device_id, expected_message)
     service.disconnect()
 
-    log_message("wait for event to arrive at device client ")
-    c2d_message_queue = device_client.receive_c2d()
-    received_c2d_message = wait_for_c2d_message(c2d_message_queue, 40, environment.leaf_device_id, expected_message)
+    time.sleep(2)
 
-    if not received_c2d_message:
+    result_message = c2d_message_queue.get()
+    if not result_message:
         log_message("Message not received")
         assert False
 
@@ -41,17 +42,16 @@ def test_device_receive_c2d_from_iothub():
     device_client.disconnect()
 
 
-def wait_for_c2d_message(c2d_message_queue, timeout, device_id, expected=None):
+def wait_for_c2d_message(c2d_message_queue, timeout, device_id, result_message=None, expected=None):
     log_message("DeviceAPI: waiting for c2d at {}".format(device_id))
     start_time = time.time()
-    received_c2d_message = c2d_message_queue.get()  # blocking cal
-    return received_c2d_message
-    # while (time.time() - start_time) < timeout:
-    #     received_c2d_message = c2d_message_queue.get()  # blocking call
-    #     log_message("The message received was " + received_c2d_message)
-    #     if expected == received_c2d_message:
-    #         log_message("DeviceAPI: message received as expected")
-    #         return received_c2d_message
-    #     else:
-    #         log_message("DeviceAPI: unexpected message.  skipping")
-    # return received_c2d_message
+    while (time.time() - start_time) < timeout:
+        received_c2d_message = c2d_message_queue.get()
+        actual = str(received_c2d_message.data, "utf-8")
+        log_message("The message received was " + actual)
+        if expected == actual:
+            log_message("DeviceAPI: message received as expected")
+            result_message = actual
+        else:
+            log_message("DeviceAPI: unexpected message.  skipping")
+    return result_message
