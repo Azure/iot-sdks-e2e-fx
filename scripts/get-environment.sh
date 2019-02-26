d77897b0ae0e3c4bcd9df43f28f85b451e228c6c
Bert Kleewein <bertk@microsoft.com>
2019-02-26 13:51:16 -0800

    force re-fetch of device_id when getting environment

--- scripts/get-environment.sh
+++ scripts/get-environment.sh
@@ -1,4 +1,6 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 export IOTHUB_E2E_EDGEHUB_CA_CERT=$(sudo cat /var/lib/iotedge/hsm/certs/edge_owner_ca*.pem | base64 -w 0)
+# force re-fetch of the device ID
+unset IOTHUB_E2E_EDGEHUB_DEVICE_ID 
 python3 $(dirname "$0")/../pyscripts/get_environment_variables.py $1
