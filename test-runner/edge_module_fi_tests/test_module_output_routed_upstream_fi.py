88b0c46d3109d46290f06a54dbe2a297ae97daba
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-03-07 11:42:00 -0800

    remove skip

--- test-runner/edge_module_fi_tests/test_module_output_routed_upstream_fi.py
+++ test-runner/edge_module_fi_tests/test_module_output_routed_upstream_fi.py
@@ -24,8 +24,7 @@ Send Output Event
 There's a rule in the routing table that sends the 'telemetry' output event to eventhub
 """
 
-# We are going to be skipping this fault injection test for now... 
-@pytest.mark.skip
+
 @pytest.mark.callsSendOutputEvent
 @pytest.mark.testgroup_edgehub_fault_injection
 def test_module_output_routed_upstream_fi():
