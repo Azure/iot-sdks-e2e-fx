adfd73bbe32fd3cee03aff3fdc240e5cd91cff6d
Bert Kleewein <BertKleewein@users.noreply.github.com>
2019-03-14 10:22:26 -0700

    add option to remove timeout when debugging containers (#23)

---
+++ test-runner/adapters/adapter_config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+
+# default timeout for all rest API calls, not otherwise specified
+default_api_timeout = 45
+
+# timeout for print_message calls
+print_message_timeout = 2
