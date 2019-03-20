c31c9c5fbf8bef1d93aa8c2f581e9a436da30a5f
Bert Kleewein <bertk@microsoft.com>
2019-03-20 15:49:15 -0700

    refactor runtime config to be more data driven

--- test-runner/runtime_config.py
+++ test-runner/runtime_config.py
@@ -16,30 +16,7 @@ import adapters
 import scenarios
 
 
-class EdgeHubRuntimeConfig:
-    def __init__(self):
-        self.service = None
-        self.registry = None
-        self.iotedge = None
-        self.test_module = None
-        self.friend_module = None
-        self.leaf_device = None
-        self.eventhub = None
-        self.ca_certificate = None
-
-
-class IotHubRuntimeConfig:
-    def __init__(self):
-        self.service = None
-        self.registry = None
-        self.test_module = None
-        self.eventhub = None
-
-
 runtime_config = None
-
-iothub_scenarios = ["iothub_module", "iothub_device"]
-edgehub_scenarios = ["edgehub_module", "edgehub_module_fi"]
 
 
 def get_current_config():
@@ -50,11 +27,10 @@ def get_current_config():
 def set_runtime_configuration(scenario, language, transport, local):
     global runtime_config
 
-    runtime_config = EdgeHubRuntimeConfig()
-
-    direct_to_iothub = True
-    if scenarios.USE_IOTEDGE_GATEWAYHOST in scenario.scenario_flags:
-        direct_to_iothub = False
+    if language == "ppdirect":
+        runtime_config = runtime_config_templates.get_runtime_config_direct(scenario)
+    else:
+        runtime_config = runtime_config_templates.get_runtime_config(scenario)
 
     ensure_edge_environment_variables()
 
@@ -64,146 +40,120 @@ def set_runtime_configuration(scenario, language, transport, local):
     # deviceId for your edgeHub instance
     edge_device_id = os.environ["IOTHUB_E2E_EDGEHUB_DEVICE_ID"]
 
+    # Are we using a gateway?  If so, collect the bits we need
+    use_gateway_host = False
+    if scenarios.USE_IOTEDGE_GATEWAYHOST in scenario.scenario_flags:
+        use_gateway_host = True
+        gateway_host_name = os.environ["IOTHUB_E2E_EDGEHUB_DNS_NAME"]
+        gateway_host_suffix = ";GatewayHostName=" + gateway_host_name
+        if "IOTHUB_E2E_EDGEHUB_CA_CERT" in os.environ:
+            runtime_config.ca_certificate = {
+                "cert": base64.b64decode(
+                    os.environ["IOTHUB_E2E_EDGEHUB_CA_CERT"]
+                ).decode("utf-8")
+            }
+        else:
+            raise Exception(
+                "IOTHUB_E2E_EDGEHUB_CA_CERT missing from environment.  Do you need to run `eval $(../scripts/get-environment.sh)`?"
+            )
+
     # DNS name for host that is running your edge hub instance
     host_for_rest_uri = os.environ["IOTHUB_E2E_EDGEHUB_DNS_NAME"]
-    gateway_host_name = os.environ["IOTHUB_E2E_EDGEHUB_DNS_NAME"]
-
     # If we're on the actual machine, just use localhost instead
-
     config_yaml = Path("/etc/iotedge/config.yaml")
     if config_yaml.is_file():
         host_for_rest_uri = "localhost"
 
-    # CA certificate for you edgeHub instance.
-    # only used in some SDKs if friend_module_connect_from_environment == False
-    #
-    # Other SDKs may need this added to the "Trusted Root Certificate Authorities"
-    # portion of the trust store if you're using connect_from_environment == False.
-    # (You know who you are)
-    #
-    # retrived from edge-e2e/scripts/get_ca_cert.sh
-    if "IOTHUB_E2E_EDGEHUB_CA_CERT" in os.environ:
-        runtime_config.ca_certificate = {
-            "cert": base64.b64decode(os.environ["IOTHUB_E2E_EDGEHUB_CA_CERT"]).decode(
-                "utf-8"
-            )
-        }
-
-    runtime_config.iotedge = runtime_config_templates.IotEdgeDevice()
-    runtime_config.friend_module = runtime_config_templates.EdgeHubModuleRest(
-        "FriendModuleClient"
-    )
-    runtime_config.eventhub = runtime_config_templates.EventHubDirect()
-
-    if language == "ppdirect":
-        container_under_test = all_containers["pythonpreview"]
-        runtime_config.service = runtime_config_templates.IotHubServiceDirect()
-        runtime_config.registry = runtime_config_templates.IotHubRegistryDirect()
-        runtime_config.test_module = runtime_config_templates.EdgeHubModuleDirect(
-            "TestModuleClient"
-        )
-        runtime_config.leaf_device = runtime_config_templates.EdgeHubLeafDeviceRest(
-            "LeafDeviceClient"
-        )
-    else:
-        container_under_test = all_containers[language]
-        runtime_config.service = runtime_config_templates.IotHubServiceRest()
-        runtime_config.registry = runtime_config_templates.IotHubRegistryRest()
-        runtime_config.test_module = runtime_config_templates.EdgeHubModuleRest(
-            "TestModuleClient"
-        )
-        runtime_config.leaf_device = runtime_config_templates.EdgeHubLeafDeviceRest(
-            "LeafDeviceClient"
-        )
-
-    hub = edgehub_factory.useExistingHubInstance(
-        service_connection_string, edge_device_id
-    )
-
+    # set up eventhub, the service client, and the registry client
     runtime_config.service.connection_string = service_connection_string
     runtime_config.registry.connection_string = service_connection_string
     runtime_config.eventhub.connection_string = service_connection_string
 
-    runtime_config.iotedge.device_id = edge_device_id
-
-    runtime_config.test_module.device_id = edge_device_id
-    runtime_config.test_module.module_id = container_under_test.module_id
-    runtime_config.test_module.connection_string = (
-        container_under_test.connection_string
-    )
-    if local:
-        runtime_config.test_module.rest_uri = "http://localhost:" + str(
-            container_under_test.local_port
-        )
-    else:
-        runtime_config.test_module.rest_uri = (
-            "http://" + host_for_rest_uri + ":" + str(container_under_test.host_port)
-        )
-    runtime_config.test_module.transport = transport
-    runtime_config.test_module.language = language
-
-    runtime_config.friend_module.device_id = edge_device_id
-    runtime_config.friend_module.module_id = all_containers["friend"].module_id
-    runtime_config.friend_module.connection_string = all_containers[
-        "friend"
-    ].connection_string
-    runtime_config.friend_module.rest_uri = (
-        "http://" + host_for_rest_uri + ":" + str(all_containers["friend"].host_port)
-    )
-
-    runtime_config.leaf_device.device_id = hub.leaf_device_id
-    runtime_config.leaf_device.connection_string = hub.leaf_device_connection_string
-    if container_under_test.deviceImpl:
-        runtime_config.leaf_device.rest_uri = runtime_config.test_module.rest_uri
-    else:
-        runtime_config.leaf_device.rest_uri = runtime_config.friend_module.rest_uri
-
-    if not runtime_config.test_module.connection_string:
-        raise Exception(
-            "test module has not been deployed.  You need to deploy your langauge module (even if you're testing locally)"
-        )
-    if not runtime_config.friend_module.connection_string:
-        raise Exception("friend module has not been deployed.")
-    if not runtime_config.leaf_device.connection_string:
-        raise Exception(
-            "Leaf device does not appear to have an iothub identity.  You may need to re-run create-new-edgehub-device.sh"
-        )
-
-    if container_under_test.registryImpl:
-        runtime_config.registry.rest_uri = runtime_config.test_module.rest_uri
-    else:
-        runtime_config.registry.rest_uri = runtime_config.friend_module.rest_uri
-
-    if container_under_test.serviceImpl:
-        runtime_config.service.rest_uri = runtime_config.test_module.rest_uri
-    else:
-        runtime_config.service.rest_uri = runtime_config.friend_module.rest_uri
-
-    if not direct_to_iothub:
-        # route all of our devices through edgeHub if necessary
-        gatewayHostSuffix = ";GatewayHostName=" + gateway_host_name
-        runtime_config.test_module.connection_string += gatewayHostSuffix
-        runtime_config.friend_module.connection_string += gatewayHostSuffix
-        runtime_config.leaf_device.connection_string += gatewayHostSuffix
-    else:
-        runtime_config.ca_certificate = {}  # must be an empty dictionary
-        runtime_config.test_module.connection_type = (
-            runtime_config_templates.CONNECTION_STRING
-        )
-        runtime_config.friend_module.connection_type = (
-            runtime_config_templates.CONNECTION_STRING
-        )
-
-    if local and not direct_to_iothub:
-        runtime_config.test_module.connection_type = (
-            runtime_config_templates.CONNECTION_STRING
-        )
-
+    # set up the device and module adapters that we're going to use
     if language == "ppdirect":
-        if not direct_to_iothub:
+        container_under_test = all_containers["pythonpreview"]
+    else:
+        container_under_test = all_containers[language]
+
+    hub = edgehub_factory.useExistingHubInstance(
+        service_connection_string, edge_device_id
+    )
+    if getattr(runtime_config, "iotedge", None):
+        runtime_config.iotedge.device_id = edge_device_id
+
+    if getattr(runtime_config, "test_module", None):
+        runtime_config.test_module.device_id = edge_device_id
+        runtime_config.test_module.module_id = container_under_test.module_id
+        runtime_config.test_module.connection_string = (
+            container_under_test.connection_string
+        )
+        if use_gateway_host:
+            runtime_config.test_module.connection_string += gateway_host_suffix
+        if local:
+            runtime_config.test_module.rest_uri = "http://localhost:" + str(
+                container_under_test.local_port
+            )
+        else:
+            runtime_config.test_module.rest_uri = (
+                "http://"
+                + host_for_rest_uri
+                + ":"
+                + str(container_under_test.host_port)
+            )
+        runtime_config.test_module.transport = transport
+        if not runtime_config.test_module.connection_string:
+            raise Exception(
+                "test module has not been deployed.  You need to deploy your langauge module (even if you're testing locally)"
+            )
+        if local:
             runtime_config.test_module.connection_type = (
                 runtime_config_templates.CONNECTION_STRING
             )
+
+    friend_mod_rest_uri = (
+        "http://" + host_for_rest_uri + ":" + str(all_containers["friend"].host_port)
+    )
+    if getattr(runtime_config, "friend_module", None):
+        runtime_config.friend_module.device_id = edge_device_id
+        runtime_config.friend_module.module_id = all_containers["friend"].module_id
+        runtime_config.friend_module.connection_string = all_containers[
+            "friend"
+        ].connection_string
+        if use_gateway_host:
+            runtime_config.friend_module.connection_string += gateway_host_suffix
+        runtime_config.friend_module.rest_uri = friend_mod_rest_uri
+        if not runtime_config.friend_module.connection_string:
+            raise Exception("friend module has not been deployed.")
+
+    if getattr(runtime_config, "leaf_device", None):
+        runtime_config.leaf_device.device_id = hub.leaf_device_id
+        runtime_config.leaf_device.connection_string = hub.leaf_device_connection_string
+        if use_gateway_host:
+            runtime_config.leaf_device.connection_string += gateway_host_suffix
+        if container_under_test.deviceImpl:
+            runtime_config.leaf_device.rest_uri = runtime_config.test_module.rest_uri
+        else:
+            runtime_config.leaf_device.rest_uri = runtime_config.friend_module.rest_uri
+        if not runtime_config.leaf_device.connection_string:
+            raise Exception(
+                "Leaf device does not appear to have an iothub identity.  You may need to re-run create-new-edgehub-device.sh"
+            )
+
+    if language != "ppdirect":
+        if container_under_test.registryImpl:
+            runtime_config.registry.rest_uri = runtime_config.test_module.rest_uri
+        else:
+            runtime_config.registry.rest_uri = friend_mod_rest_uri
+
+        if container_under_test.serviceImpl:
+            runtime_config.service.rest_uri = runtime_config.test_module.rest_uri
+        else:
+            runtime_config.service.rest_uri = friend_mod_rest_uri
+
+    if language == "ppdirect":
+        runtime_config.test_module.connection_type = (
+            runtime_config_templates.CONNECTION_STRING
+        )
 
     for object_name in dir(runtime_config):
         test_obj = getattr(runtime_config, object_name)
--- test-runner/scenarios.py
+++ test-runner/scenarios.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# connectino!/usr/bin/env python
 
 # --------------------------------------------------------------------------
 # Deployment types
@@ -19,9 +19,22 @@ valid_scenario_flags = [CONNECT_WITH_ENVIRONMENT, USE_IOTEDGE_GATEWAYHOST]
 # --------------------------------------------------------------------------
 # HortonScenario object
 
+EDGEHUB_MODULE = "edgehub_module"
+IOTHUB_MODULE = "iothub_module"
+EDGEHUB_MODULE_FI = "edgehub_module_fi"
+IOTHUB_MODULE_AND_DEVICE = "iothub_module_and_device"
+
+valid_scenarios = [
+    EDGEHUB_MODULE,
+    IOTHUB_MODULE,
+    EDGEHUB_MODULE_FI,
+    IOTHUB_MODULE_AND_DEVICE,
+]
+
 
 class HortonScenario:
-    def __init__(self, deployment_type, pytest_markers, scenario_flags):
+    def __init__(self, scenario_name, deployment_type, pytest_markers, scenario_flags):
+        self.scenario_name = scenario_name
         self.deployment_type = deployment_type
         self.pytest_markers = pytest_markers
         self.scenario_flags = scenario_flags
@@ -29,17 +42,30 @@ class HortonScenario:
 
 scenarios = {
     "edgehub_module": HortonScenario(
+        scenario_name=EDGEHUB_MODULE,
         deployment_type=EDGEHUB_DEPLOYMENT,
         pytest_markers=["testgroup_edgehub_module_client"],
         scenario_flags=[CONNECT_WITH_ENVIRONMENT, USE_IOTEDGE_GATEWAYHOST],
     ),
     "iothub_module": HortonScenario(
+        scenario_name=IOTHUB_MODULE,
         deployment_type=IOTHUB_DEPLOYMENT,
-        pytest_markers=["testgroup_iothub_module_client"], scenario_flags=[]
+        pytest_markers=["testgroup_iothub_module_client"],
+        scenario_flags=[],
     ),
     "edgehub_module_fi": HortonScenario(
+        scenario_name=EDGEHUB_MODULE_FI,
         deployment_type=EDGEHUB_DEPLOYMENT,
         pytest_markers=["testgroup_edgehub_fault_injection"],
         scenario_flags=[CONNECT_WITH_ENVIRONMENT, USE_IOTEDGE_GATEWAYHOST],
+    ),
+    "iothub_module_and_device": HortonScenario(
+        scenario_name=IOTHUB_MODULE_AND_DEVICE,
+        deployment_type=IOTHUB_DEPLOYMENT,
+        pytest_markers=[
+            "testgroup_iothub_module_client",
+            "testgroup_iothub_device_client",
+        ],
+        scenario_flags=[],
     ),
 }
