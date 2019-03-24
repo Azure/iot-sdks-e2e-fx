6ac7d89cff6a3fa7d94cab5c8d5a52a469af4599
Bert Kleewein <BertKleewein@users.noreply.github.com>
2019-03-24 08:37:52 -0700

    add device telemetry, update pythonpreview wrapper with newly generated code (#35)

--- ci-wrappers/pythonpreview/wrapper/swagger_server/controllers/service_controller.py
+++ ci-wrappers/pythonpreview/wrapper/swagger_server/controllers/service_controller.py
@@ -5,12 +5,12 @@ from swagger_server.models.connect_response import ConnectResponse  # noqa: E501
 from swagger_server import util
 
 
-def service_connect_put(connectionString):  # noqa: E501
+def service_connect(connectionString):  # noqa: E501
     """Connect to service
 
     Connect to the Azure IoTHub service.  More specifically, the SDK saves the connection string that is passed in for future use. # noqa: E501
 
-    :param connectionString: Service connection string
+    :param connectionString: connection string
     :type connectionString: str
 
     :rtype: ConnectResponse
@@ -18,7 +18,20 @@ def service_connect_put(connectionString):  # noqa: E501
     return 'do some magic!'
 
 
-def service_connection_id_device_method_device_id_put(connectionId, deviceId, methodInvokeParameters):  # noqa: E501
+def service_disconnect(connectionId):  # noqa: E501
+    """Disconnect from the service
+
+    Disconnects from the Azure IoTHub service.  More specifically, closes all connections and cleans up all resources for the active connection # noqa: E501
+
+    :param connectionId: Id for the connection
+    :type connectionId: str
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def service_invoke_device_method(connectionId, deviceId, methodInvokeParameters):  # noqa: E501
     """call the given method on the given device
 
      # noqa: E501
@@ -35,20 +48,7 @@ def service_connection_id_device_method_device_id_put(connectionId, deviceId, me
     return 'do some magic!'
 
 
-def service_connection_id_disconnect_put(connectionId):  # noqa: E501
-    """Disconnect from the service
-
-    Disconnects from the Azure IoTHub service.  More specifically, closes all connections and cleans up all resources for the active connection # noqa: E501
-
-    :param connectionId: Id for the connection
-    :type connectionId: str
-
-    :rtype: None
-    """
-    return 'do some magic!'
-
-
-def service_connection_id_module_method_device_id_module_id_put(connectionId, deviceId, moduleId, methodInvokeParameters):  # noqa: E501
+def service_invoke_module_method(connectionId, deviceId, moduleId, methodInvokeParameters):  # noqa: E501
     """call the given method on the given module
 
      # noqa: E501
--- ci-wrappers/pythonpreview/wrapper/swagger_server/controllers/wrapper_controller.py
+++ ci-wrappers/pythonpreview/wrapper/swagger_server/controllers/wrapper_controller.py
@@ -4,7 +4,7 @@ import six
 from swagger_server import util
 import wrapper_glue
 
-def wrapper_cleanup_put():  # noqa: E501
+def wrapper_cleanup():  # noqa: E501
     """verify that the clients have cleaned themselves up completely
 
      # noqa: E501
@@ -15,8 +15,8 @@ def wrapper_cleanup_put():  # noqa: E501
     wrapper_glue.cleanup_resources()
 
 
-def wrapper_configuration_get():  # noqa: E501
-    """gets configuration details on the current wrapper configuration
+def wrapper_get_capabilities():  # noqa: E501
+    """Get capabilities for this test wrapper
 
      # noqa: E501
 
@@ -26,37 +26,15 @@ def wrapper_configuration_get():  # noqa: E501
     return 'do some magic!'
 
 
-def wrapper_message_put(msg):  # noqa: E501
+def wrapper_log_message(msg):  # noqa: E501
     """log a message to output
 
      # noqa: E501
 
-    :param msg:
-    :type msg:
+    :param msg: 
+    :type msg: 
 
     :rtype: None
     """
     wrapper_glue.log_message(msg)
 
-
-
-def wrapper_session_get():  # noqa: E501
-    """Terminate a wrapper, optionally returning the log
-
-     # noqa: E501
-
-
-    :rtype: None
-    """
-    return 'do some magic!'
-
-
-def wrapper_session_put():  # noqa: E501
-    """Launch a wrapper, getting ready to test
-
-     # noqa: E501
-
-
-    :rtype: None
-    """
-    return 'do some magic!'
