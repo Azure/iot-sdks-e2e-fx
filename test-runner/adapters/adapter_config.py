0d47ab56f153b7a01593b5d57b6eb05ecd6adf18
Bert Kleewein <bertk@microsoft.com>
2019-04-17 12:45:08 -0700

    bump timeout to 60 seconds

--- test-runner/adapters/adapter_config.py
+++ test-runner/adapters/adapter_config.py
@@ -5,7 +5,7 @@
 # full license information.
 
 # default timeout for all rest API calls, not otherwise specified
-default_api_timeout = 45
+default_api_timeout = 60
 
 # timeout for print_message calls
 print_message_timeout = 2
