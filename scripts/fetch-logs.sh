dbffb59e82a445e582b2c47e5e00614bda74c2ac
Gregg Acheson <gregg.acheson@oslosolutions.com>
2019-06-03 16:05:34 -0700

    merge script

--- scripts/fetch-logs.sh
+++ scripts/fetch-logs.sh
@@ -34,4 +34,9 @@ if [ $? -ne 0 ]; then
   echo "error merging logs"
 fi
 
+echo "injecting merged.log into junit"
+pushd $resultsdir && python ${root_dir}/pyscripts/inject_into_junit.py -junit_file $2 -log_file merged.log
+if [ $? -ne 0 ]; then
+  echo "error injecting into junit"
+fi
 
