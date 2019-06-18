49db65dc5d4a483ffe374be3cb379092fd97d065
gregga <gregg.acheson@oslosolutions.com>
2019-06-18 13:40:22 -0700

    x docker and colorama

--- scripts/install-pyscript-reqs.ps1
+++ scripts/install-pyscript-reqs.ps1
@@ -11,5 +11,5 @@ $pyscripts = Join-Path -Path $root_dir -ChildPath 'pyscripts' -Resolve
 
 $py = PyCmd-Run "-m pip install --upgrade pip"; Invoke-Expression  $py
 $py = PyCmd-Run "-m pip install -r $pyscripts/requirements.txt"; Invoke-Expression  $py
-$py = PyCmd-Run "-m pip install docker"; Invoke-Expression  $py
-$py = PyCmd-Run "-m pip install colorama"; Invoke-Expression  $py
+#$py = PyCmd-Run "-m pip install docker"; Invoke-Expression  $py
+#$py = PyCmd-Run "-m pip install colorama"; Invoke-Expression  $py
