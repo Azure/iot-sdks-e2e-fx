bbd1c67423e205a70a62921ea560c5f5f5f550d1
gregga <gregg.acheson@oslosolutions.com>
2019-06-18 13:24:31 -0700

    install-pyscript-reqs

---
+++ scripts/install-pyscript-reqs.ps1
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+$path = $MyInvocation.MyCommand.Path
+if (!$path) {$path = $psISE.CurrentFile.Fullpath}
+if ( $path) {$path = split-path $path -Parent}
+. $path/pwsh-helpers.ps1
+
+$root_dir = Join-Path -Path $path -ChildPath '..' -Resolve
+$pyscripts = Join-Path -Path $root_dir -ChildPath 'pyscripts' -Resolve
+
+$py = PyCmd-Run "-m pip install --upgrade pip"; Invoke-Expression  $py
+$py = PyCmd-Run "-m pip install -r $pyscripts/requirements.txt"; Invoke-Expression  $py
+$py = PyCmd-Run "-m pip install docker"; Invoke-Expression  $py
+$py = PyCmd-Run "-m pip install colorama"; Invoke-Expression  $py
