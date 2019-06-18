c5d9795df95a4c92cdc33c7bab2cba51c446200f
gregga <gregg.acheson@oslosolutions.com>
2019-06-18 14:44:45 -0700

    setup-iotedge

---
+++ scripts/setup/setup-iotedge.ps1
@@ -0,0 +1,19 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+$path = $MyInvocation.MyCommand.Path
+if (!$path) {$path = $psISE.CurrentFile.Fullpath}
+if ( $path) {$path = split-path $path -Parent}
+$root_dir = Join-Path -Path $path -ChildPath '../..' -Resolve
+$scripts = Join-Path -Path $root_dir -ChildPath 'scripts' -Resolve
+. $scripts/pwsh-helpers.ps1
+$isWin32 = IsWin32
+
+#$py = PyCmd-Run "-m pip install --upgrade pip"; Invoke-Expression  $py
+#$py = PyCmd-Run "-m pip install --upgrade setuptools"; Invoke-Expression  $py
+
+if($isWin32 -eq $false) {
+    Write-Host "Installing iotedge..." -ForegroundColor Yellow
+    sudo apt-get install -y iotedge
+    sudo chmod 666 /etc/iotedge/config.yaml
+}
