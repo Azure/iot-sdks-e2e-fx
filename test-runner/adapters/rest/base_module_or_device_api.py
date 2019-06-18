30a64251430e35a504917d429517692e19bcb6cd
Bert Kleewein <bertk@microsoft.com>
2019-06-18 15:59:32 -0700

    sleep 10 seconds before disconnect

--- test-runner/adapters/rest/base_module_or_device_api.py
+++ test-runner/adapters/rest/base_module_or_device_api.py
@@ -26,14 +26,14 @@ class BaseModuleOrDeviceApi:
     def disconnect(self):
         if self.connection_id:
             # give client a chance to finish up any IO (such as sending input message ACK)
-            time.sleep(2)
+            time.sleep(10)
             self.rest_endpoint.disconnect(
                 self.connection_id, timeout=adapter_config.default_api_timeout
             )
             self.connection_id = ""
             # give edgeHub a chance to disconnect MessagingServiceClient from IoTHub.  It does this lazily after the module disconnects from edgeHub
+            time.sleep(2)
 
-            time.sleep(10)
 
     @log_entry_and_exit
     def enable_twin(self):
