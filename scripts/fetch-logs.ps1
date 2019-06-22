0a12fde2cb57e3c22c2a42768a15ea122d165fef
gregga <gregg.acheson@oslosolutions.com>
2019-06-21 17:41:44 -0700

    more logging

--- scripts/fetch-logs.ps1
+++ scripts/fetch-logs.ps1
@@ -48,19 +48,25 @@ foreach($mod in $modulelist) {
     if("$mod" -ne "") {
         $modFile ="$resultsdir/$mod.log"
         $modulefiles += $modFile
-        if(Test-Path $modFile) {
-            Remove-Item $modFile
-        }
-        Write-Host "Getting log for $modFile" -ForegroundColor Green
+        #if(Test-Path $modFile) {
+        #    Remove-Item $modFile
+        #}
+        Write-Host "Getting log for $mod" -ForegroundColor Green
         #$dkr_log = Invoke-Cmd "sudo docker logs -t $mod"
         #Invoke-Cmd "docker logs -t $mod"
         #$dkr_log = Invoke-Expression "sudo docker logs -t $mod" 4>&1 >> $modFile
-        $dkr_cmd = "sudo docker logs -t $mod"
+        if($isWin32)  {
+            $dkr_cmd = "docker logs -t $mod"
+        }
+        else {
+            $dkr_cmd = "sudo docker logs -t $mod 4>&1"
+        }
         #$dkr_cmd = "docker image ls"
         #$dkr_log = Invoke-Expression $dkr_cmd 
         Invoke-Expression $dkr_cmd | Out-File $modFile
         #Get-Content -Path $modFile | Measure-Object -Line
-        #Get-Content -Path $modFile
+        Write-Host "#######################################"
+        Get-Content -Path $modFile
         #$dkr_log = ( Invoke-Expression $dkr_cmd ) | Out-File $modFile
         #$dlen = $dkr_log.length
         #Write-Host "($modFile)($dlen)"
