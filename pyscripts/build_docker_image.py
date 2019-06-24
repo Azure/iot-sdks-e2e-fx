981ac4c3f02fbbc010fa6f45648b4388e03098a3
gregga <gregg.acheson@oslosolutions.com>
2019-06-24 15:58:22 -0700

    Updates of feedback from PR#79

--- pyscripts/build_docker_image.py
+++ pyscripts/build_docker_image.py
@@ -10,237 +10,207 @@ import datetime
 import colorama
 from colorama import Fore
 
-class BuildDockerImage:
+colorama.init(autoreset=True)
 
-    def __init__(self, args):
+default_repo = "(Azure/azure-iot-sdk-BLAH)"
+all_languages = ["c", "csharp", "python", "pythonpreview", "node", "java"]
 
-        colorama.init(autoreset=True)
+parser = argparse.ArgumentParser(description="build docker image for testing")
+parser.add_argument(
+    "--language",
+    help="language to build",
+    type=str,
+    required=True,
+    choices=all_languages,
+)
+parser.add_argument("--repo", help="repo with source", type=str, default=default_repo)
+parser.add_argument(
+    "--commit", help="commit to apply (ref or branch)", type=str, default="master"
+)
+parser.add_argument(
+    "--variant", help="Docker image variant (blank for default)", type=str, nargs="?", const=""
+)
+args = parser.parse_args()
 
-        default_repo = "(Azure/azure-iot-sdk-BLAH)"
-        all_languages = ["c", "csharp", "python", "pythonpreview", "node", "java"]
+if args.repo == default_repo:
+    if args.language == "pythonpreview":
+        args.repo = "Azure/azure-iot-sdk-python-preview"
+    else:
+        args.repo = "Azure/azure-iot-sdk-" + args.language
+    print(Fore.YELLOW + "Repo not specified.  Defaulting to " + args.repo)
 
-        parser = argparse.ArgumentParser(description="build docker image for testing")
-        parser.add_argument(
-            "--language",
-            help="language to build",
-            type=str,
-            required=True,
-            choices=all_languages,
-        )
-        parser.add_argument("--repo", help="repo with source", type=str, default=default_repo)
-        parser.add_argument(
-            "--commit", help="commit to apply (ref or branch)", type=str, default="master"
-        )
-        parser.add_argument(
-            "--variant", help="Docker image variant (blank for default)", type=str, nargs="?", const=""
-        )
-        args = parser.parse_args()
+print_separator = "".join("/\\" for _ in range(80))
 
-        if args.repo == default_repo:
-            if args.language == "pythonpreview":
-                args.repo = "Azure/azure-iot-sdk-python-preview"
-            else:
-                args.repo = "Azure/azure-iot-sdk-" + args.language
-            print(Fore.YELLOW + "Repo not specified.  Defaulting to " + args.repo)
+if sys.platform == 'win32':
+    base_url = "tcp://127.0.0.1:2375"
+else:
+    base_url = "unix://var/run/docker.sock"
 
-        print_separator = "".join("/\\" for _ in range(80))
-
-        auth_config = {
-            "username": os.environ["IOTHUB_E2E_REPO_USER"],
-            "password": os.environ["IOTHUB_E2E_REPO_PASSWORD"],
-        }
-
-
-        def get_dockerfile_directory(tags):
-            script_dir = os.path.dirname(os.path.realpath(__file__))
-            return os.path.normpath(os.path.join(script_dir, "../ci-wrappers/" + tags.language))
+auth_config = {
+    "username": os.environ["IOTHUB_E2E_REPO_USER"],
+    "password": os.environ["IOTHUB_E2E_REPO_PASSWORD"],
+}
 
 
-        def print_filtered_docker_line(lineobj):
-            lineobj = lineobj.splitlines()
-            for line in lineobj:
-                try:
-                    linecode = line.strip().decode("utf-8")
-                    linecode = linecode[:linecode.rfind("\r\n")] + '}'
-                    jline = json.loads(linecode)
-                    obj = jline["stream"]
-                except:
-                    print(''.join([chr(i) if i < 128 else '\x23' for i in line]))
+def get_dockerfile_directory(tags):
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.normpath(os.path.join(script_dir, "../ci-wrappers/" + tags.language))
+
+
+def print_filtered_docker_line(line):
+    try:
+        obj = json.loads(line.decode("utf-8"))
+    except:
+        print("".join([i if ord(i) < 128 else "#" for i in line.decode("utf-8")]))
+    else:
+        if "status" in obj:
+            if "id" in obj:
+                if obj["status"] not in [
+                    "Waiting",
+                    "Downloading",
+                    "Verifying Checksum",
+                    "Extracting",
+                    "Preparing",
+                    "Pushing",
+                ]:
+                    print("{}: {}".format(obj["status"], obj["id"]))
                 else:
-                    if "status" in obj:
-                        if "id" in obj:
-                            if obj["status"] not in [
-                                "Waiting",
-                                "Downloading",
-                                "Verifying Checksum",
-                                "Extracting",
-                                "Preparing",
-                                "Pushing",
-                            ]:
-                                print("{}: {}".format(obj["status"], obj["id"]))
-                            else:
-                                pass
-                        else:
-                            print(obj["status"])
-                    elif "error" in obj:
-                        print ("docker error: {}".format(line))
-                        raise Exception(obj["error"])
-                    elif "Step" in obj or "---" in obj:
-                        print("{}".format(obj).strip())
-                    elif obj == "\n":
-                        pass
-                    else:
-                        lineout = "".format("{}", line)
-                        print(''.join([chr(i) if i < 128 else '\x23' for i in lineout]))
-        
-
-        def build_image(tags):
-            print(print_separator)
-            print("BUILDING IMAGE")
-            print(print_separator)
-
-            force_flag = 0
-
-            if sys.platform == 'win32':
-                base_url = "tcp://127.0.0.1:2375"
+                    pass
             else:
-                base_url = "unix://var/run/docker.sock"
-            api_client = docker.APIClient(base_url=base_url)
+                print(obj["status"])
+        elif "error" in obj:
+            print ("docker error: {}".format(line))
+            raise Exception(obj["error"])
+        elif "Step" in obj or "---" in obj:
+            print("step: {}".format(obj).strip())
+        elif obj == "\n":
+            pass
+        else:
+            print("".join([i if ord(i) < 128 else "#" for i in line.decode("utf-8")]))
 
-            build_args = {
-                "HORTON_REPO": tags.repo,
-                "HORTON_COMMIT_NAME": tags.commit_name,
-                "HORTON_COMMIT_SHA": tags.commit_sha,
-            }
 
-            if tags.image_tag_to_use_for_cache:
-                cache_from = [
-                    tags.docker_full_image_name + ":" + tags.image_tag_to_use_for_cache
-                ]
-                print("using {} for cache".format(cache_from[0]))
-            else:
-                cache_from = []
+def build_image(tags):
+    print(print_separator)
+    print("BUILDING IMAGE")
+    print(print_separator)
 
-            if tags.variant:
-                dockerfile = "Dockerfile." + tags.variant
-            else:
-                dockerfile = "Dockerfile"
+    force_flag = 0
+    api_client = docker.APIClient(base_url=base_url)
 
-            print(
-                Fore.YELLOW
-                + "Building image for "
-                + tags.docker_image_name
-                + " using "
-                + dockerfile
-            )
-            for line in api_client.build(
-                path=get_dockerfile_directory(tags),
-                tag=tags.docker_image_name,
-                buildargs=build_args,
-                cache_from=cache_from,
-                dockerfile=dockerfile,
-            ):
+    build_args = {
+        "HORTON_REPO": tags.repo,
+        "HORTON_COMMIT_NAME": tags.commit_name,
+        "HORTON_COMMIT_SHA": tags.commit_sha,
+    }
+
+    if tags.image_tag_to_use_for_cache:
+        cache_from = [
+            tags.docker_full_image_name + ":" + tags.image_tag_to_use_for_cache
+        ]
+        print("using {} for cache".format(cache_from[0]))
+    else:
+        cache_from = []
+
+    if tags.variant:
+        dockerfile = "Dockerfile." + tags.variant
+    else:
+        dockerfile = "Dockerfile"
+
+    print(
+        Fore.YELLOW
+        + "Building image for "
+        + tags.docker_image_name
+        + " using "
+        + dockerfile
+    )
+    for line in api_client.build(
+        path=get_dockerfile_directory(tags),
+        tag=tags.docker_image_name,
+        buildargs=build_args,
+        cache_from=cache_from,
+        dockerfile=dockerfile,
+    ):
+        print_filtered_docker_line(line)
+
+def tag_images(tags):
+    print(print_separator)
+    print("TAGGING IMAGE")
+    print(print_separator)
+
+    api_client = docker.APIClient(base_url=base_url)
+
+    print("Adding tags")
+    for image_tag in tags.image_tags:
+        print("Adding " + image_tag)
+        api_client.tag(tags.docker_image_name, tags.docker_full_image_name, image_tag.lower())
+
+
+def push_images(tags):
+    print(print_separator)
+    print("PUSHING IMAGE")
+    print(print_separator)
+
+    api_client = docker.APIClient(base_url=base_url)
+
+    for image_tag in tags.image_tags:
+        print("Pushing {}:{}".format(tags.docker_full_image_name, image_tag))
+        for line in api_client.push(
+            tags.docker_full_image_name, image_tag.lower(), stream=True, auth_config=auth_config
+        ):
+            try:
+                sys.stdout.write(json.loads(line.decode("utf-8"))["stream"])
+            except:
                 print_filtered_docker_line(line)
 
-        def tag_images(tags):
-            print(print_separator)
-            print("TAGGING IMAGE")
-            print(print_separator)
 
-            if sys.platform == 'win32':
-                base_url = "tcp://127.0.0.1:2375"
-            else:
-                base_url = "unix://var/run/docker.sock"
-            api_client = docker.APIClient(base_url=base_url)
+def prefetch_cached_images(tags):
+    if docker_tags.running_on_azure_pipelines():
+        print(print_separator)
+        print(Fore.YELLOW + "PREFETCHING IMAGE")
+        print(print_separator)
+        tags.image_tag_to_use_for_cache = None
 
-            print("Adding tags")
-            for image_tag in tags.image_tags:
-                print("Adding " + image_tag)
-                api_client.tag(tags.docker_image_name, tags.docker_full_image_name, image_tag.lower())
+        api_client = docker.APIClient(base_url=base_url)
 
-
-        def push_images(tags):
-            print(print_separator)
-            print("PUSHING IMAGE")
-            print(print_separator)
-
-            if sys.platform == 'win32':
-                base_url = "tcp://127.0.0.1:2375"
-            else:
-                base_url = "unix://var/run/docker.sock"
-            api_client = docker.APIClient(base_url=base_url)
-
-            for image_tag in tags.image_tags:
-                print("Pushing {}:{}".format(tags.docker_full_image_name, image_tag))
-                for line in api_client.push(
-                    tags.docker_full_image_name, image_tag.lower(), stream=True, auth_config=auth_config
-                ):
-                    try:
-                        sys.stdout.write(json.loads(line.decode("utf-8"))["stream"])
-                    except:
-                        try:
-                            print_filtered_docker_line(line)
-                        except:
-                            out_line = ''.format("{}",line)
-                            print(''.join([i if ord(i) < 128 else '#' for i in out_line]))
-
-
-        def prefetch_cached_images(tags):
-            if docker_tags.running_on_azure_pipelines():
-                print(print_separator)
-                print(Fore.YELLOW + "PREFETCHING IMAGE")
-                print(print_separator)
-                tags.image_tag_to_use_for_cache = None
-
-                if sys.platform == 'win32':
-                    base_url = "tcp://127.0.0.1:2375"
-                else:
-                    base_url = "unix://var/run/docker.sock"
-                api_client = docker.APIClient(base_url=base_url)
-        
-                for image_tag in tags.image_tags:
-                    print(
-                        Fore.YELLOW
-                        + "trying to prefetch {}:{}".format(
-                            tags.docker_full_image_name, image_tag
-                        )
-                    )
-                    try:
-                        for line in api_client.pull(
-                            tags.docker_full_image_name,
-                            image_tag,
-                            stream=True,
-                            auth_config=auth_config,
-                        ):
-                            print_filtered_docker_line(line)
-                        tags.image_tag_to_use_for_cache = image_tag
-                        print(
-                            Fore.GREEN
-                            + "Found {}.  Using this for image cache".format(image_tag)
-                        )
-                        return
-                    except docker.errors.APIError:
-                        print(Fore.YELLOW + "Image not found in repository")
-
-
-        tags = docker_tags.get_docker_tags_from_commit(
-            args.language, args.repo, args.commit, args.variant
-        )
-
-        prefetch_cached_images(tags)
-        build_image(tags)
-        tag_images(tags)
-        push_images(tags)
-
-        if not docker_tags.running_on_azure_pipelines():
-            print(Fore.GREEN + "Done.  Deploy with the following command:")
+        for image_tag in tags.image_tags:
             print(
-                Fore.GREEN
-                + "./deploy-test-containers.sh --{} {}:{}".format(
-                    tags.language, tags.docker_full_image_name, tags.image_tags[0]
+                Fore.YELLOW
+                + "trying to prefetch {}:{}".format(
+                    tags.docker_full_image_name, image_tag
                 )
             )
+            try:
+                for line in api_client.pull(
+                    tags.docker_full_image_name,
+                    image_tag,
+                    stream=True,
+                    auth_config=auth_config,
+                ):
+                    print_filtered_docker_line(line)
+                tags.image_tag_to_use_for_cache = image_tag
+                print(
+                    Fore.GREEN
+                    + "Found {}.  Using this for image cache".format(image_tag)
+                )
+                return
+            except docker.errors.APIError:
+                print(Fore.YELLOW + "Image not found in repository")
 
-if __name__ == "__main__":
-    build_image = BuildDockerImage(sys.argv[1:])
-    
+
+tags = docker_tags.get_docker_tags_from_commit(
+    args.language, args.repo, args.commit, args.variant
+)
+
+prefetch_cached_images(tags)
+build_image(tags)
+tag_images(tags)
+push_images(tags)
+
+if not docker_tags.running_on_azure_pipelines():
+    print(Fore.GREEN + "Done.  Deploy with the following command:")
+    print(
+        Fore.GREEN
+        + "./deploy-test-containers.sh --{} {}:{}".format(
+            tags.language, tags.docker_full_image_name, tags.image_tags[0]
+        )
+    )    
--- pyscripts/docker_log_processor.py
+++ pyscripts/docker_log_processor.py
@@ -246,7 +246,6 @@ class DockerLogProcessor:
                             # Made it past filters and PyTest, so Log the line
                             log_line_parts = log_line.split("Z ")
                             if log_line_parts:
-                                valid_line = False
                                 log_data = ""
                                 num_parts = len(log_line_parts)
 
@@ -260,12 +259,10 @@ class DockerLogProcessor:
                                 if num_parts >= 2:
                                     try:
                                         log_time = DockerLogProcessor.format_date_and_time(log_line_parts[0], "%Y-%m-%d %H:%M:%S.%f")
-                                        valid_line = True
-                                    except:
-                                        print("INVALID_TIMESTAMP({}):{}".format(module_name, log_line))
-                                    if valid_line:
                                         log_line_object = LogLineObject(log_time, module_name, log_data)
                                         loglines.append(log_line_object)
+                                    except:
+                                        print("INVALID_TIMESTAMP({}):{}".format(module_name, log_line))
                                 else:
                                     print("INVALID_LINE({}):{}".format(module_name, log_line))
 
@@ -319,4 +316,3 @@ class LogLineObject:
 
 if __name__ == "__main__":
     log_processor = DockerLogProcessor(sys.argv[1:])
-    
