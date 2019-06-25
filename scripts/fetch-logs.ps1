505e142b54a4e225921c8747fab63eea30b7323b
gregga <gregg.acheson@oslosolutions.com>
2019-06-25 15:10:34 -0700

    WORKING - Code cleanup

--- scripts/fetch-logs.ps1
+++ scripts/fetch-logs.ps1
@@ -21,28 +21,17 @@ $isWin32 = IsWin32
 $log_folder_name = $log_folder_name.trim("/")
 $root_dir = Join-Path -Path $path -ChildPath '..' -Resolve
 $pyscripts = Join-Path -Path $root_dir -ChildPath 'pyscripts' -Resolve
-
+$resultsdir="$build_dir/results/logs"
 
 Write-Host "Fetching Logs for $log_folder_name in: $build_dir" -ForegroundColor Green
-
-#resultsdir=$root_dir/results/logs
-#mkdir -p $resultsdir
-#$resultsdir="$build_dir/results/$log_folder_name"
-$resultsdir="$build_dir/results/logs"
 
 if(Test-Path -Path $resultsdir)
 {
     Get-ChildItem -Path "$resultsdir/*" -Recurse | Remove-Item -Force -Recurse
     Remove-Item -Path $resultsdir -Force -Recurse
-    #New-Item -ItemType directory -Path $build_dir/results
-    #New-Item -ItemType directory -Path $build_dir/results/logs
     New-Item -ItemType directory -Path $resultsdir
 }
-else {
-    #New-Item -ItemType directory -Path $build_dir/results
-    #New-Item -ItemType directory -Path $build_dir/results/logs
-    New-Item -ItemType directory -Path $resultsdir
-}
+New-Item -ItemType directory -Path $resultsdir
 
 $languageMod = $langmod + "Mod"
 $modulefiles = @()
@@ -73,20 +62,8 @@ $junit_file = "$build_dir/TEST-$log_folder_name.xml"
 Write-Host "injecting merged.log into junit: $junit_file" -ForegroundColor Green
 Invoke-PyCmd "$pyscripts/inject_into_junit.py -junit_file $junit_file -log_file $resultsdir/merged.log"
 
-#mkdir -p $(Build.SourcesDirectory)/results/${{ parameters.log_folder_name }}
-#mv $(Horton.FrameworkRoot)/results/logs/* $(Build.SourcesDirectory)/results/${{ parameters.log_folder_name }}/
-#mv $(Build.SourcesDirectory)/TEST-* $(Build.SourcesDirectory)/results
-
 New-Item -ItemType directory -Path $build_dir/results/$log_folder_name
 Move-Item "$resultsdir/*" $build_dir/results/$log_folder_name
-
-#mv "$build_dir/TEST-*" "$build_dir/results"
-#if(Test-Path -Path $junit_file )
-#{
-#    Copy-Item $junit_file -Destination "$build_dir/results"
-#}
-
-
 $files = Get-ChildItem "$build_dir/TEST_*"
 if($files) {
     Move-Item $files "$build_dir/results"
