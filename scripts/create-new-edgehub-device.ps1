b3166186c63a2f5fc3f6376c2b202163eb8cbba8
Gregg Acheson <gregg.acheson@oslosolutions.com>
2019-07-18 13:08:08 -0700

    update build pipeline to powershell (#87)
    * pyscripts updates for impending pwsh pipeline
    
    * add inject-int-junit to PR
    
    * Updates of feedback from PR#79
    
    * Add pwsh-helpers
    
    * Add pwsh scripts
    
    * update yaml for pwsh pipeline
    
    * changes to build_docker_image.py as requested in PR#79 feedback
    
    * copy dir to artifacts
    
    * make results and results/logs dirs
    
    * update run-pytest-module.ps1
    
    * show build_dir
    
    * fix results path
    
    * update results dir path
    
    * use Move-Item
    
    * copy junit_file
    
    * catch junit update
    
    * WORKING - Code cleanup

---
+++ scripts/create-new-edgehub-device.ps1
@@ -0,0 +1,20 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+$path = $MyInvocation.MyCommand.Path
+if (!$path) {$path = $psISE.CurrentFile.Fullpath}
+if ( $path) {$path = split-path $path -Parent}
+. $path/pwsh-helpers.ps1
+$isWin32 = IsWin32
+$pyscripts = Join-Path -Path $path -ChildPath '../pyscripts' -Resolve
+$hh = Join-Path -Path $path -ChildPath '../horton_helpers' -Resolve
+
+if($isWin32 -eq $false) {
+    Invoke-PyCmd "-m pip install -e $hh"
+}
+
+Invoke-PyCmd "$pyscripts/create_new_edgehub_device.py"
+
+if($isWin32 -eq $false) {
+    sudo systemctl restart iotedge
+}
---
+++ scripts/verify-container-running.ps1
@@ -0,0 +1,18 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+Param
+(
+    [Parameter(Position=0)]
+    [string]$container_name
+)
+
+$path = $MyInvocation.MyCommand.Path
+if (!$path) {$path = $psISE.CurrentFile.Fullpath}
+if ( $path) {$path = split-path $path -Parent}
+. $path/pwsh-helpers.ps1
+$root_dir = Join-Path -Path $path -ChildPath '..' -Resolve
+$pyscripts = Join-Path -Path $root_dir -ChildPath 'pyscripts' -Resolve
+
+Invoke-PyCmd "$pyscripts/ensure-container.py $container_name"
+ 
