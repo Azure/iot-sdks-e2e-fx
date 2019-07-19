664eb56ec5a1ef5f78eefe0d05f3aee2d076d62c
Bert Kleewein <BertKleewein@users.noreply.github.com>
2019-07-19 09:34:44 -0700

    clean up scripts (#88)

--- scripts/Tag-DockerImage.ps1
+++ scripts/Tag-DockerImage.ps1
@@ -1,4 +1,4 @@
-#!/usr/bin/env powershell
+#!/usr/bin/env pwsh
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for
 # full license information.
