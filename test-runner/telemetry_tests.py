4a63cb425ba3a95ce14d5d542451f9e5b98f2ee0
Bert Kleewein <bertk@microsoft.com>
2019-09-19 06:29:15 -0700

    rename tests

--- test-runner/telemetry_tests.py
+++ test-runner/telemetry_tests.py
@@ -13,7 +13,7 @@ class TelemetryTests(object):
     @pytest.mark.timeout(
         timeout=180
     )  # extra timeout in case eventhub needs to retry due to resource error
-    def test_send_event_to_iothub(self, client, eventhub, test_object_stringified):
+    def test_send_telemetry_to_iothub(self, client, eventhub, test_object_stringified):
         client.send_event(test_object_stringified)
 
         received_message = eventhub.wait_for_next_event(
@@ -29,7 +29,9 @@ class TelemetryTests(object):
     @pytest.mark.it(
         "Can send telemetry directly to IoTHub using the new Horton HubEvent"
     )
-    def test_device_send_string_using_new_message_format(self, client, eventhub, body):
+    def test_device_send_telemetry_using_new_message_format(
+        self, client, eventhub, body
+    ):
         sent_message = HubEvent(body)
         client.send_event(sent_message.convert_to_json())
 
