7b418200ef416561c22ce7433a560763097a6503
Bert Kleewein <bertk@microsoft.com>
2019-10-19 08:27:23 -0700

    add separators

--- test-runner/fixtures.py
+++ test-runner/fixtures.py
@@ -43,58 +43,69 @@ def logger():
     return print_message
 
 
+dashes = "".join(("-" for _ in range(0, 30)))
+separator = "{} CLEANUP {} {}".format(dashes, "{}", dashes)
+
+
 @pytest.fixture
-def eventhub():
+def eventhub(logger):
     eventhub = connections.connect_eventhub_client()
     yield eventhub
+    logger(separator.format("eventhub"))
     eventhub.disconnect_sync()
 
 
 @pytest.fixture
-def registry():
+def registry(logger):
     registry = connections.connect_registry_client()
     yield registry
+    logger(separator.format("registry"))
     registry.disconnect_sync()
 
 
 @pytest.fixture
-def friend():
+def friend(logger):
     friend = connections.connect_friend_module_client()
     friend.device_id = get_current_config().friend_module.device_id
     friend.module_id = get_current_config().friend_module.module_id
     yield friend
+    logger(separator.format("friend module"))
     friend.disconnect_sync()
 
 
 @pytest.fixture
-def test_module():
+def test_module(logger):
     test_module = connections.connect_test_module_client()
     test_module.device_id = get_current_config().test_module.device_id
     test_module.module_id = get_current_config().test_module.module_id
     yield test_module
+    logger(separator.format("test module"))
     test_module.disconnect_sync()
 
 
 @pytest.fixture
-def leaf_device():
+def leaf_device(logger):
     leaf_device = connections.connect_leaf_device_client()
     leaf_device.device_id = get_current_config().leaf_device.device_id
     yield leaf_device
+    logger(separator.format("leaf device"))
     leaf_device.disconnect_sync()
 
 
 @pytest.fixture
-def test_device():
+def test_device(logger):
     test_device = connections.connect_test_device_client()
     test_device.device_id = get_current_config().test_device.device_id
     yield test_device
+    logger(separator.format("device"))
     test_device.disconnect_sync()
 
 
 @pytest.fixture
-def service():
+def service(logger):
     service = connections.connect_service_client()
     yield service
+    logger(separator.format("service"))
     service.disconnect_sync()
 
 
