66e959486a3d5e96e802b99735fefccaefe8cdeb
Bert Kleewein <bertk@microsoft.com>
2019-10-22 12:35:13 -0700

    copy changes from retry branch

--- test-runner/telemetry_tests.py
+++ test-runner/telemetry_tests.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license. See LICENSE file in the project root for
 # full license information.
 
+import asyncio
 import pytest
 import json
 from models import HubEvent
@@ -41,3 +42,45 @@ class TelemetryTests(object):
             client.device_id, expected=sent_message.body
         )
         assert received_message is not None, "Message not received"
+
+    @pytest.mark.skip("#BKTODO: Only in python for now.")
+    @pytest.mark.it("Can send 5 telemetry events directly to iothub")
+    async def test_send_5_telemetry_events_to_iothub(
+        self, client, eventhub, sample_payload, logger
+    ):
+        payloads = [sample_payload() for x in range(0, 5)]
+        futures = []
+
+        # start listening before we send
+        received_message_future = asyncio.ensure_future(
+            eventhub.wait_for_next_event(client.device_id)
+        )
+
+        for payload in payloads:
+            futures.append(asyncio.ensure_future(client.send_event(payload)))
+
+        # wait for the send to complete, and verify that it arrvies
+        await asyncio.gather(*futures)
+
+        logger("All messages sent.  Awaiting reception")
+
+        while len(payloads):
+            received_message = await received_message_future
+
+            if received_message in payloads:
+                logger(
+                    "Received expected message: {}, removing from list".format(
+                        received_message
+                    )
+                )
+                payloads.remove(received_message)
+            else:
+                logger(
+                    "Received unexpected message: {}, removing from list".format(
+                        received_message
+                    )
+                )
+
+            received_message_future = asyncio.ensure_future(
+                eventhub.wait_for_next_event(client.device_id)
+            )
