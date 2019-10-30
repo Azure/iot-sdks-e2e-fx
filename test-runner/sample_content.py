bf8d9927716a6d797fe8b5a1fe188d7639fcfa8f
Bert Kleewein <BertKleewein@users.noreply.github.com>
2019-10-30 12:48:19 -0700

    remove some sample content to make python runs faster (#152)

--- test-runner/sample_content.py
+++ test-runner/sample_content.py
@@ -6,14 +6,8 @@ from utilities import random_string
 import pytest
 import json
 
-string_64 = random_string("64-chars", length=64)
 string_1K = random_string(prefix="1K-chars", length=1024)
-string_2000 = random_string(prefix="2000-chars", length=2000)
 string_64K = random_string(prefix="64K-chars", length=64 * 1024)
-
-simple_object = {"64_chars": random_string(length=64)}
-simple_object_stringified = json.dumps(simple_object)
-
 object_with_3_layers = {
     "layer_1_string": random_string("1"),
     "layer_2": {
@@ -21,19 +15,11 @@ object_with_3_layers = {
         "layer3": {"fake_number": 3, "fake_real_number": 1.87},
     },
 }
-object_with_3_layers_stringified = json.dumps(object_with_3_layers)
 
 telemetry_test_objects = [
-    pytest.param(string_64, id="64 character string"),
     pytest.param(string_1K, id="1K character string"),
-    pytest.param(string_2000, id="2000 character string"),
     pytest.param(string_64K, id="64k character string"),
-    pytest.param(simple_object, id="simple object"),
-    pytest.param(simple_object_stringified, id="simple object, as string"),
     pytest.param(object_with_3_layers, id="object with 3 layers"),
-    pytest.param(
-        object_with_3_layers_stringified, id="object with 3 layers, as string"
-    ),
 ]
 
 string_index = 0
