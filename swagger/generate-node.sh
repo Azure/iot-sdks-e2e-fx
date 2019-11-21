f79b3f42ada7d334bb1fb970968578ea9f2aa0e7
Bert Kleewein <bertk@microsoft.com>
2019-11-21 15:30:43 -0800

    checkpoint

---
+++ swagger/generate-node.sh
@@ -0,0 +1,41 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+script_dir=$(cd "$(dirname "$0")" && pwd)
+root_dir=$(cd "${script_dir}/.." && pwd)
+source "$script_dir/../scripts/colorecho.sh"
+
+colorecho $_red "WARNING: This script overwrites code.  If you have anything checked out, it might be destroyed by this script."
+colorecho $_red "Do you wish to run this anyway?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
+
+cd ${root_dir}/swagger
+[ $? -eq 0 ] || { echo "cd swagger failed"; exit 1; }
+
+rm -r swagger_generated/node
+# OK to fail
+
+./generate.sh node
+[ $? -eq 0 ] || { echo "generate.sh failed"; exit 1; }
+
+colorecho $_yellow "copying generated files"
+
+exit 0
+
+cd ${root_dir}
+[ $? -eq 0 ] || { echo "cd ${root_dir} failed"; exit 1; }
+
+rm -r docker_images/pythonv2/wrapper/swagger_server
+[ $? -eq 0 ] || { echo "rm swagger_swerver failed"; exit 1; }
+
+cp -r swagger/swagger_generated/pythonv2/swagger_server/ docker_images/pythonv2/wrapper/swagger_server/
+[ $? -eq 0 ] || { echo "cp failed"; exit 1; }
+
+rm -r docker_images/pythonv2/wrapper/swagger_server/test/
+[ $? -eq 0 ] || { echo "rm test failed"; exit 1; }
+
+colorecho $_green "SUCCESS!"
