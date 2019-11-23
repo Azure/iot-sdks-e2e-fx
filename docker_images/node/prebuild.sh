bbdd381dbee1d351a7a27218e199e4ac83892335
Bert Kleewein <bertk@microsoft.com>
2019-11-23 06:42:27 -0800

    use e2efx wrapper for node

--- docker_images/node/prebuild.sh
+++ docker_images/node/prebuild.sh
@@ -1,11 +1,20 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-cd /sdk
-[ $? -eq 0 ] || { echo "cd shared failed"; exit 1; }
+cd /src
+[ $? -eq 0 ] || { echo "cd src failed"; exit 1; }
+
+cp sdk/package.json .
+[ $? -eq 0 ] || { echo "cp package.json failed"; exit 1; }
 
 npm install
 [ $? -eq 0 ] || { echo "npm install failed"; exit 1; }
+
+cp sdk/lerna.json .
+[ $? -eq 0 ] || { echo "cp lerna.json failed"; exit 1; }
+
+node /fixLerna.js
+[ $? -eq 0 ] || { echo "fixLerna.js failed"; exit 1; }
 
 lerna bootstrap --scope iot-sdk-device-client-rest-api --include-filtered-dependencies --hoist
 [ $? -eq 0 ] || { echo "lerna bootstrap failed"; exit 1; }
--- docker_images/node/rebuild.sh
+++ docker_images/node/rebuild.sh
@@ -1,8 +1,14 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-cd /sdk
-[ $? -eq 0 ] || { echo "cd shared failed"; exit 1; }
+cd /src
+[ $? -eq 0 ] || { echo "cd src failed"; exit 1; }
+
+cp sdk/lerna.json .
+[ $? -eq 0 ] || { echo "cp lerna.json failed"; exit 1; }
+
+node /fixLerna.js
+[ $? -eq 0 ] || { echo "fixLerna.js failed"; exit 1; }
 
 lerna bootstrap --scope iot-sdk-device-client-rest-api --include-filtered-dependencies --hoist
 [ $? -eq 0 ] || { echo "lerna bootstrap failed"; exit 1; }
