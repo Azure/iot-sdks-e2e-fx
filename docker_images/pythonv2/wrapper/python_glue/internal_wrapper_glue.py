49da30a8a4e70bdfdafb527082fc4ba72feaacf5
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-12-09 08:42:28 -0800

    remove

--- docker_images/pythonv2/wrapper/python_glue/internal_wrapper_glue.py
+++ docker_images/pythonv2/wrapper/python_glue/internal_wrapper_glue.py
@@ -39,5 +39,5 @@ def get_capabilities():
             "dropped_connection_tests": True,
             "net_control_app": True,
         },
-        "skip_list": [  'callsSendOutputEvent', 'receivesInputMessages', 'receivesMethodCalls', 'supportsTwin' ]
+        "skip_list": []
     }
