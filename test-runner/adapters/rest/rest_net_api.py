2d727884aa9987ee10a201295caee1ab6cf0d171
Bert Kleewein <bertk@microsoft.com>
2020-04-02 10:05:57 -0700

    use aio autorest apis

--- test-runner/adapters/rest/rest_net_api.py
+++ test-runner/adapters/rest/rest_net_api.py
@@ -1,51 +1,53 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for
 # full license information.
-from .generated.e2erestapi import AzureIOTEndToEndTestWrapperRestApi
+from .generated import e2erestapi
 import msrest
 from .. import adapter_config
 from ..abstract_net_api import AbstractNetApi
 from .rest_decorators import log_entry_and_exit
-from ..decorators import control_api_emulate_async
 
 
 class NetApi(AbstractNetApi):
     def __init__(self, hostname):
-        self.rest_endpoint = AzureIOTEndToEndTestWrapperRestApi(hostname).net
+        self.rest_endpoint = e2erestapi.aio.AzureIOTEndToEndTestWrapperRestApi(
+            hostname
+        ).net
         self.rest_endpoint.config.retry_policy.retries = 0
+
+        self.rest_endpoint_sync = e2erestapi.AzureIOTEndToEndTestWrapperRestApi(
+            hostname
+        ).net
+        self.rest_endpoint_sync.config.retry_policy.retries = 0
 
     @log_entry_and_exit
     def set_destination_sync(self, ip, transport):
-        self.rest_endpoint.set_destination(
+        self.rest_endpoint_sync.set_destination(
             ip, transport, timeout=adapter_config.control_api_timeout
         )
 
-    @control_api_emulate_async
     @log_entry_and_exit
-    def disconnect(self, disconnect_type):
-        self.rest_endpoint.disconnect(
+    async def disconnect(self, disconnect_type):
+        await self.rest_endpoint.disconnect(
             disconnect_type, timeout=adapter_config.control_api_timeout
         )
 
-    @control_api_emulate_async
     @log_entry_and_exit
-    def reconnect(self):
-        self.rest_endpoint.reconnect(timeout=adapter_config.control_api_timeout)
+    async def reconnect(self):
+        await self.rest_endpoint.reconnect(timeout=adapter_config.control_api_timeout)
 
     @log_entry_and_exit
     def reconnect_sync(self):
-        self.rest_endpoint.reconnect(timeout=adapter_config.control_api_timeout)
+        self.rest_endpoint_sync.reconnect(timeout=adapter_config.control_api_timeout)
 
-    @control_api_emulate_async
     @log_entry_and_exit
-    def disconnect_after_c2d(self, disconnect_type):
-        self.rest_endpoint.disconnect_after_c2d(
+    async def disconnect_after_c2d(self, disconnect_type):
+        await self.rest_endpoint.disconnect_after_c2d(
             disconnect_type, timeout=adapter_config.control_api_timeout
         )
 
-    @control_api_emulate_async
     @log_entry_and_exit
-    def disconnect_after_d2c(self, disconnect_type):
-        self.rest_endpoint.disconnect_after_d2c(
+    async def disconnect_after_d2c(self, disconnect_type):
+        await self.rest_endpoint.disconnect_after_d2c(
             disconnect_type, timeout=adapter_config.control_api_timeout
         )
