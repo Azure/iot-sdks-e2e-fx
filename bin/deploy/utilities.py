ee5a6a7e5d15e000d43375d93e68bf9a610212b8
Bert Kleewein <BertKleewein@users.noreply.github.com>
2020-04-02 07:23:44 -0700

    feature: selectively use sudo to enable running inside containers (#257)

--- bin/deploy/utilities.py
+++ bin/deploy/utilities.py
@@ -21,22 +21,34 @@ def is_pi():
     return (not is_windows()) and (run_shell_command("uname -m")[0] == "armv7l")
 
 
+sudo_prefix_value = None
+
+
 def sudo_prefix():
-    if is_windows():
-        return ""
-    else:
-        return "sudo -n "
+    global sudo_prefix_value
+
+    if sudo_prefix_value is None:
+        if is_windows():
+            sudo_prefix_value = ""
+        else:
+            try:
+                run_shell_command("which sudo")
+            except subprocess.CalledProcessError:
+                sudo_prefix_value = ""
+            else:
+                sudo_prefix_value = "sudo -n "
+
+    return sudo_prefix_value
+
+
+def run_elevated_shell_command(cmd):
+    return run_shell_command(sudo_prefix() + cmd)
 
 
 def run_shell_command(cmd):
-    cmd_with_prefix = sudo_prefix() + cmd
-    print("running [{}]".format(cmd_with_prefix))
+    print("running [{}]".format(cmd))
     try:
-        return (
-            subprocess.check_output(cmd_with_prefix.split(" "))
-            .decode("utf-8")
-            .splitlines()
-        )
+        return subprocess.check_output(cmd.split(" ")).decode("utf-8").splitlines()
     except subprocess.CalledProcessError as e:
         print("Error spawning {}".format(e.cmd))
         print("Process returned {}".format(e.returncode))
@@ -110,20 +122,20 @@ def remove_instance(settings_object):
 
 def try_remove_container(container_name):
     try:
-        run_shell_command("docker stop {}".format(container_name))
-    except subprocess.CalledProcessError:
+        run_elevated_shell_command("docker stop {}".format(container_name))
+    except (subprocess.CalledProcessError, FileNotFoundError):
         print("Ignoring failure")
     try:
-        run_shell_command("docker rm {}".format(container_name))
-    except subprocess.CalledProcessError:
+        run_elevated_shell_command("docker rm {}".format(container_name))
+    except (subprocess.CalledProcessError, FileNotFoundError):
         print("Ignoring failure")
 
 
 def remove_old_instances():
     if not is_windows():
         try:
-            run_shell_command("systemctl stop iotedge")
-        except subprocess.CalledProcessError:
+            run_elevated_shell_command("systemctl stop iotedge")
+        except (subprocess.CalledProcessError, FileNotFoundError):
             print("Ignoring failure")
 
     if settings.test_module.container_name:
@@ -138,7 +150,7 @@ def remove_old_instances():
 
 def pull_docker_image(image):
     if image != PYTHON_INPROC:
-        run_shell_command("docker pull {image}".format(image=image))
+        run_elevated_shell_command("docker pull {image}".format(image=image))
 
 
 def create_docker_container(obj):
@@ -146,7 +158,7 @@ def create_docker_container(obj):
 
     # hp_x = host port (port as seen by host OS)
     # cp_x = container port (port as exposed from app inside container)
-    run_shell_command(
+    run_elevated_shell_command(
         "docker run -d -p {hp_1}:{cp_1} -p {hp_2}:{cp_2} -p {hp_3}:{cp_3} --name {name} --restart=on-failure:10 --cap-add NET_ADMIN --cap-add NET_RAW {image}".format(
             hp_1=obj.host_port,
             cp_1=obj.container_port,
