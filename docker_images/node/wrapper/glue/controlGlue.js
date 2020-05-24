e1e886f05b7642c5667633fad35998ba6d3bbeb8
Bert Kleewein <BertKleewein@users.noreply.github.com>
2020-05-24 07:57:44 -0700

    Feature: finish node connect changes for net-drop testing (#276)

--- docker_images/node/wrapper/glue/controlGlue.js
+++ docker_images/node/wrapper/glue/controlGlue.js
@@ -75,6 +75,7 @@ exports.control_GetCapabilities = function() {
     debug("returning capabilities");
     var caps = {
       'flags': {
+        'v2_connect_group': true
       },
       'skip_list': []
     };
