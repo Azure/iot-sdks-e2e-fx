bea206b5994a5265d7b6f49bb376fe3171175ed3
Bert Kleewein <bertk@microsoft.com>
2020-07-13 09:58:07 -0700

    squash

--- test-runner/longhaul_config.py
+++ test-runner/longhaul_config.py
@@ -1,88 +1,22 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information
 import datetime
-from dictionary_object import SimpleObject, DictionaryObject
+from dictionary_object import DictionaryObject
 
 
-class Platform(SimpleObject):
-    def __init__(self):
-        super(Platform, self).__init__()
-        self.os = ""
-        self.framework_version = ""
-
-
-class OpConfig(SimpleObject):
-    def __init__(self):
-        super(OpConfig, self).__init__()
-        self.enabled = False
-        self.interval_length = 0
-        self.ops_per_interval = 0
-
-
-class TestConfig(SimpleObject):
+class TestConfig(DictionaryObject):
     def __init__(self):
         super(TestConfig, self).__init__()
-        self.total_duration = datetime.timedelta()
-        self.timeout_interval = datetime.timedelta(minutes=2)
-        self.reporting_interval = datetime.timedelta(seconds=10)
-        self.telemetry_interval = datetime.timedelta(seconds=2)
-        self.eventhub_renew_interval = datetime.timedelta(minutes=15)
-        self.max_allowed_failures = 0
-        self.d2c = OpConfig()
-
-
-class OpStatus(SimpleObject):
-    def __init__(self):
-        super(OpStatus, self).__init__()
-        self.ops_completed = 0
-        self.ops_failed = 0
-        self.ops_sending = 0
-        self.ops_verifying = 0
-        self.max_send_latency = 0
-        self.max_verify_latency = 0
-
-
-class TestStatus(SimpleObject):
-    def __init__(self):
-        super(TestStatus, self).__init__()
-
-        self.status = "new"
-        self.start_time = datetime.datetime.min
-        self.elapsed_time = datetime.timedelta(0)
-        self.total_ops_failed = 0
-
-        self.d2c = OpStatus()
-
-
-class IntervalReport(DictionaryObject):
-    def __init__(self):
-        super(IntervalReport, self).__init__()
-        self.interval_id = 0
-        self.objects_in_pytest_process = 0
-        self.d2c = OpStatus()
+        self.test_run_total_duration = datetime.timedelta()
+        self.test_run_timeout_interval = datetime.timedelta(minutes=2)
+        self.test_run_reporting_interval = datetime.timedelta(seconds=10)
+        self.test_run_telemetry_interval = datetime.timedelta(seconds=2)
+        self.test_run_eventhub_renew_interval = datetime.timedelta(minutes=1)
+        self.test_run_max_allowed_failures = 0
+        self.d2c_enabled = False
+        self.d2c_interval_length = 1
+        self.d2c_ops_per_interval = 10
         self.lock_attributes()
 
 
-IntervalReport._defaults = IntervalReport()
-
-
-class DesiredTestProperties(DictionaryObject):
-    def __init__(self):
-        super(DesiredTestProperties, self).__init__()
-        self.test_config = TestConfig()
-        self.lock_attributes()
-
-
-DesiredTestProperties._defaults = DesiredTestProperties()
-
-
-class ReportedTestProperties(DictionaryObject):
-    def __init__(self):
-        super(ReportedTestProperties, self).__init__()
-        self.platform = Platform()
-        self.test_config = TestConfig()
-        self.test_status = TestStatus()
-        self.lock_attributes()
-
-
-ReportedTestProperties._defaults = ReportedTestProperties()
+TestConfig._defaults = TestConfig()
---
+++ test-runner/longhaul_telemetry.py
@@ -0,0 +1,72 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information
+import datetime
+from dictionary_object import DictionaryObject
+
+
+class PlatformProperties(DictionaryObject):
+    def __init__(self):
+        self.os = ""
+
+        self.horton_repo = ""
+        self.horton_branch = ""
+        self.horton_uri = ""
+
+        self.sdk_repo = ""
+        self.sdk_branch = ""
+        self.sdk_uri = ""
+        self.lock_attributes()
+
+
+PlatformProperties._defaults = PlatformProperties()
+
+
+class TestRunProperties(DictionaryObject):
+    def __init__(self):
+        super(TestRunProperties, self).__init__()
+        self.test_run_status = "new"
+        self.test_run_start_time = datetime.datetime.min
+        self.test_run_elapsed_time = datetime.timedelta(0)
+        self.test_run_total_ops_failed = 0
+        self.lock_attributes()
+
+
+class PlatformTelemetry(DictionaryObject):
+    def __init__(self):
+        super(PlatformTelemetry, self).__init__()
+        self.procinfo_ram_total = 0
+        self.procinfo_ram_used = 0
+        self.procinfo_ram_free = 0
+        self.system_uptime = 0
+        self.user_time = 0
+        self.system_time = 0
+        self.lock_attributes()
+
+
+PlatformTelemetry._defaults = PlatformTelemetry()
+
+
+class TestRunnerTelemetry(DictionaryObject):
+    def __init__(self):
+        super(TestRunnerTelemetry, self).__init__()
+        self.test_gc_object_count = 0
+        self.sdk_gc_object_count = 0
+        self.lock_attributes()
+
+
+TestRunnerTelemetry._defaults = TestRunnerTelemetry()
+
+
+class D2cTelemetry(DictionaryObject):
+    def __init__(self):
+        super(D2cTelemetry, self).__init__()
+        self.count_total_d2c_completed = 0
+        self.count_total_d2c_failed = 0
+        self.count_current_d2c_sending = 0
+        self.count_current_d2c_verifying = 0
+        self.latency_d2c_send = 0
+        self.latency_d2c_verify = 0
+        self.lock_attributes()
+
+
+D2cTelemetry.__defaults = D2cTelemetry()
