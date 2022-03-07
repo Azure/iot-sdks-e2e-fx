891e7e0a1947c22e9f94893ffd6f29ccd4ef2573
timtay-microsoft <timtay@microsoft.com>
2022-03-07 11:13:39 -0800

    fixup

--- docker_images/java/wrapper/src/main/java/glue/UnixDomainSocketChannelImpl.java
+++ docker_images/java/wrapper/src/main/java/glue/UnixDomainSocketChannelImpl.java
@@ -12,23 +12,17 @@ import java.nio.ByteBuffer;
 
 public class UnixDomainSocketChannelImpl implements UnixDomainSocketChannel
 {
-    UnixSocketAddress unixSocketAddress;
-    UnixSocketChannel channel;
+    private UnixSocketChannel channel;
 
     @Override
     public void open(String address) throws IOException
     {
-        System.out.printf("Opening unix domain socket for address %s%n", address);
-        this.unixSocketAddress = new UnixSocketAddress(address);
-        this.channel = UnixSocketChannel.open(this.unixSocketAddress);
+        this.channel = UnixSocketChannel.open(new UnixSocketAddress(address));
     }
 
     @Override
     public void write(byte[] output) throws IOException
     {
-        String writeContents = new String(output.clone());
-        System.out.println("Writing content to unix domain socket:");
-        System.out.println(writeContents);
         this.channel.write(ByteBuffer.wrap(output));
     }
 
@@ -38,19 +32,12 @@ public class UnixDomainSocketChannelImpl implements UnixDomainSocketChannel
         ByteBuffer inputByteBuffer = ByteBuffer.wrap(inputBuffer);
         int bytesRead = this.channel.read(inputByteBuffer);
         System.arraycopy(inputByteBuffer.array(), 0, inputBuffer, 0, inputByteBuffer.capacity());
-
-        String readContents = new String(inputBuffer.clone());
-        System.out.println("Read " + bytesRead + " bytes from unix domain socket");
-        System.out.println("Read content from unix domain socket:");
-        System.out.println(readContents);
-
         return bytesRead;
     }
 
     @Override
     public void close() throws IOException
     {
-        System.out.println("Closing unix domain socket");
         this.channel.close();
     }
 }
