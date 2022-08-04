01f4172d41abbd37c0e0a3b9b54c88bbb987a838
Bert Kleewein <bertk@microsoft.com>
2022-08-04 12:25:59 -0700

    fix Python container builds with new folder structure

--- docker_images/pythonv2/prebuild.sh
+++ docker_images/pythonv2/prebuild.sh
@@ -5,7 +5,14 @@ set -e
 
 cd /sdk
 pip install --upgrade pip
-pip install -U --upgrade-strategy eager -e . --ignore-installed packaging
+# Temporarily commenting this out because of a structure change in the repo.
+# Once Python PR 1030 makes it into main, we can add this back in for a slight improvement
+# in image usage. This is because prebuild.sh populates the base docker image (including pip depenencies)
+# and rebuild.sh populates the image layers that change for every run. With this commented out, dependencies
+# go into per-build layers. When we add this back in, dependencies will go back into the base layers
+# that get re-used over and over again. Image size is still the same in both cases.  The only difference
+# is in the size of the per-build layers which end up taking more space in the container repository.
+# pip install -U --upgrade-strategy eager -e . --ignore-installed packaging
 cd /wrapper
 pip install -r requirements.txt
 
