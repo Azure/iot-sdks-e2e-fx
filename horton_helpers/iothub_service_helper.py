cf39eb8729c1e07e5f10f2181992149dbd6b2f19
Bert Kleewein <bertk@microsoft.com>
2022-10-13 12:24:18 -0700

    Use azure.iot.hub service SDK instead of swagger generated service code

--- horton_helpers/iothub_service_helper.py
+++ horton_helpers/iothub_service_helper.py
@@ -2,48 +2,16 @@
 # Licensed under the MIT license. See LICENSE file in the project root for
 # full license information.
 
-from autorest_service_apis.service20180630 import IotHubGatewayServiceAPIs20180630
-from autorest_service_apis.service20180630.models.configuration_content import (
+from azure.iot.hub import IoTHubRegistryManager, IoTHubConfigurationManager
+from azure.iot.hub.models import (
+    Device,
+    Module,
+    DeviceCapabilities,
     ConfigurationContent,
 )
-from autorest_service_apis.service20180630.models.device import Device
-from autorest_service_apis.service20180630.models.device_capabilities import (
-    DeviceCapabilities,
-)
-from autorest_service_apis.service20180630.models.module import Module
+
 from msrest.exceptions import HttpOperationError
 import connection_string
-import uuid
-import time
-import random
-
-max_failure_count = 5
-
-initial_backoff = 10
-
-
-def run_with_retry(fun, args, kwargs):
-    failures_left = max_failure_count
-    retry = True
-    backoff = initial_backoff + random.randint(1, 10)
-
-    while retry:
-        try:
-            return fun(*args, **kwargs)
-        except HttpOperationError as e:
-            resp = e.response.json()
-            retry = False
-            if "Message" in resp:
-                if resp["Message"].startswith("ErrorCode:ThrottlingBacklogTimeout"):
-                    retry = True
-            if retry and failures_left:
-                failures_left = failures_left - 1
-                print("{} failures left before giving up".format(failures_left))
-                print("sleeping for {} seconds".format(backoff))
-                time.sleep(backoff)
-                backoff = backoff * 2
-            else:
-                raise e
 
 
 class IoTHubServiceHelper:
@@ -51,21 +19,15 @@ class IoTHubServiceHelper:
         self.cn = connection_string.connection_string_to_sas_token(
             service_connection_string
         )
-        self.service = IotHubGatewayServiceAPIs20180630(
-            "https://" + self.cn["host"]
-        ).service
-
-    def headers(self):
-        return {
-            "Authorization": self.cn["sas"],
-            "Request-Id": str(uuid.uuid4()),
-            "User-Agent": "azure-edge-e2e",
-        }
+        self.registry_manager = IoTHubRegistryManager.from_connection_string(
+            service_connection_string
+        )
+        self.configuration_manager = IoTHubConfigurationManager.from_connection_string(
+            service_connection_string
+        )
 
     def get_device_connection_string(self, device_id):
-        device = run_with_retry(
-            self.service.get_device, (device_id,), {"custom_headers": self.headers()}
-        )
+        device = self.registry_manager.get_device(device_id)
 
         primary_key = device.authentication.symmetric_key.primary_key
         return (
@@ -78,11 +40,7 @@ class IoTHubServiceHelper:
         )
 
     def get_module_connection_string(self, device_id, module_id):
-        module = run_with_retry(
-            self.service.get_module,
-            (device_id, module_id),
-            {"custom_headers": self.headers()},
-        )
+        module = self.registry_manager.get_module(device_id, module_id)
 
         primary_key = module.authentication.symmetric_key.primary_key
         return (
@@ -99,69 +57,47 @@ class IoTHubServiceHelper:
     def apply_configuration(self, device_id, modules_content):
         content = ConfigurationContent(modules_content=modules_content)
 
-        run_with_retry(
-            self.service.apply_configuration_on_edge_device,
-            (device_id, content),
-            {"custom_headers": self.headers()},
+        self.configuration_manager.apply_configuration_on_edge_device(
+            device_id, content
         )
 
     def create_device(self, device_id, is_edge=False):
         print("creating device {}".format(device_id))
         try:
-            device = run_with_retry(
-                self.service.get_device,
-                (device_id,),
-                {"custom_headers": self.headers()},
-            )
+            device = self.registry_manager.get_device(device_id)
             print("using existing device")
         except HttpOperationError:
-            device = Device(device_id)
+            device = Device(device_id=device_id)
 
         if is_edge:
-            device.capabilities = DeviceCapabilities(True)
+            device.capabilities = DeviceCapabilities(iot_edge=True)
 
-        run_with_retry(
-            self.service.create_or_update_device,
-            (device_id, device),
-            {"custom_headers": self.headers()},
+        self.registry_manager.protocol.devices.create_or_update_identity(
+            device_id, device
         )
 
     def create_device_module(self, device_id, module_id):
         print("creating module {}/{}".format(device_id, module_id))
         try:
-            module = run_with_retry(
-                self.service.get_module,
-                (device_id, module_id),
-                {"custom_headers": self.headers()},
-            )
+            module = self.registry_manager.get_module(device_id, module_id)
             print("using existing device module")
         except HttpOperationError:
-            module = Module(module_id, None, device_id)
+            module = Module(device_id=device_id, module_id=module_id)
 
-        run_with_retry(
-            self.service.create_or_update_module,
-            (device_id, module_id, module),
-            {"custom_headers": self.headers()},
+        self.registry_manager.protocol.modules.create_or_update_identity(
+            device_id, module_id, module
         )
 
     def try_delete_device(self, device_id):
         try:
-            run_with_retry(
-                self.service.delete_device,
-                (device_id,),
-                {"if_match": "*", "custom_headers": self.headers()},
-            )
+            self.registry_manager.delete_device(device_id)
             return True
         except HttpOperationError:
             return False
 
     def try_delete_module(self, device_id, module_id):
         try:
-            run_with_retry(
-                self.service.delete_module,
-                (device_id, module_id),
-                {"if_match": "*", "custom_headers": self.headers()},
-            )
+            self.registry_manager.delete_module(device_id, module_id)
             return True
         except HttpOperationError:
             return False
