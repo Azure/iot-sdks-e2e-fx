d3a9e538e4e6090648166a0a3e4a1dc808aebbd6
Bert Kleewein <BertKleewein@users.noreply.github.com>
2022-10-13 14:09:41 -0700

    update service wrappers used during tests to use service SDK (#368)
    * update service wrappers used during tests to use service SDK
    
    * change horton_helpers to use src layout

--- test-runner/adapters/direct_azure_rest/direct_service_api.py
+++ test-runner/adapters/direct_azure_rest/direct_service_api.py
@@ -1,58 +1,42 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for
 # full license information.
-from autorest_service_apis.service20180630modified import (
-    IotHubGatewayServiceAPIs20180630 as IotHubGatewayServiceAPIs,
-)
 from ..abstract_iothub_apis import AbstractServiceApi
 from ..decorators import emulate_async
-import connection_string
-import uuid
 from .amqp_service_client import AmqpServiceClient
+from azure.iot.hub import IoTHubRegistryManager, models
 
 
 class ServiceApi(AbstractServiceApi):
     def __init__(self):
-        self.service = None
         self.service_connection_string = None
         self.amqp_service_client = None
-
-    def headers(self):
-        cn = connection_string.connection_string_to_sas_token(
-            self.service_connection_string
-        )
-        return {
-            "Authorization": cn["sas"],
-            "Request-Id": str(uuid.uuid4()),
-            "User-Agent": "azure-edge-e2e",
-        }
+        self.registry_manager = None
 
     async def connect(self, service_connection_string):
         self.service_connection_string = service_connection_string
-        host = connection_string.connection_string_to_dictionary(
+        self.registry_manager = IoTHubRegistryManager.from_connection_string(
             service_connection_string
-        )["HostName"]
-        self.service = IotHubGatewayServiceAPIs("https://" + host).service
+        )
 
     async def disconnect(self):
         if self.amqp_service_client:
             await self.amqp_service_client.disconnect()
             self.amqp_serice_client = None
-        self.service = None
+        self.registry_manager = None
 
     @emulate_async
     def call_module_method(self, device_id, module_id, method_invoke_parameters):
-        return self.service.invoke_device_method1(
+        return self.registry_manager.invoke_device_module_method(
             device_id,
             module_id,
-            method_invoke_parameters,
-            custom_headers=self.headers(),
+            models.CloudToDeviceMethod.from_dict(method_invoke_parameters),
         ).as_dict()
 
     @emulate_async
     def call_device_method(self, device_id, method_invoke_parameters):
-        return self.service.invoke_device_method(
-            device_id, method_invoke_parameters, custom_headers=self.headers()
+        return self.registry_manager.invoke_device_method(
+            device_id, models.CloudToDeviceMethod.from_dict(method_invoke_parameters)
         ).as_dict()
 
     async def send_c2d(self, device_id, message):
