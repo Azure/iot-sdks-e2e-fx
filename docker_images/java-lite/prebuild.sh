ae5c27f551a26aac7e906dc6d61e612b4d0f13d8
timtay-microsoft <timtay@microsoft.com>
2023-06-08 15:16:29 -0700

    asdf

--- docker_images/java-lite/prebuild.sh
+++ docker_images/java-lite/prebuild.sh
@@ -3,9 +3,7 @@
 
 cd /sdk
 [ $? -eq 0 ] || { echo "cd sdk failed"; exit 1; }
-
-#TODO timtay why is this project not found?
-mvn -s /usr/share/maven/ref/settings-docker.xml package
-#mvn -s /usr/share/maven/ref/settings-docker.xml -Dmaven.test.skip=true -Dmaven.javadoc.skip=true --projects :iot-edge-e2e-wrapper --also-make  install
+ls
+mvn -s /usr/share/maven/ref/settings-docker.xml -Dmaven.test.skip=true -Dmaven.javadoc.skip=true --projects :iot-edge-e2e-wrapper --also-make  install
 [ $? -eq 0 ] || { echo "build sdk failed"; exit 1; }
 
