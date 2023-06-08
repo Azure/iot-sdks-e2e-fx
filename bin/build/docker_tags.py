3793b3da7f150b2d366cb0d92b193967b1b57f47
timtay-microsoft <timtay@microsoft.com>
2023-06-08 14:35:09 -0700

    fixup

--- bin/build/docker_tags.py
+++ bin/build/docker_tags.py
@@ -81,7 +81,7 @@ def get_docker_tags_from_commit(language, repo, commit, variant):
     tags.repo = repo
     tags.commit_name = get_commit_name(commit)
     tags.commit_sha = github.get_sha_from_commit(repo, commit)
-    
+
     default_variant = get_default_variant(language)
     tags.variant = variant or default_variant
     tags.using_default_variant = tags.variant == default_variant
