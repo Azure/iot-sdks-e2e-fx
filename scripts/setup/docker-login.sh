39214d679d4e6d3e20eaafb11955cef54f05ee44
Bert Kleewein <BertKleewein@users.noreply.github.com>
2023-06-12 15:22:49 -0700

    chore: script removal and cleanup (#388)

--- scripts/setup/docker-login.sh
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-docker login -u $IOTHUB_E2E_REPO_USER -p $IOTHUB_E2E_REPO_PASSWORD $IOTHUB_E2E_REPO_ADDRESS
-[ $? -eq 0 ] || { echo "docker login failed"; exit 1; }
