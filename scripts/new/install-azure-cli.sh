24f951a25b6cff76048ad12f86754593c965b4b9
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2026-04-23 11:29:58 -0700

    Fix shell compatibility in Horton prereq and wait scripts

--- scripts/new/install-azure-cli.sh
+++ scripts/new/install-azure-cli.sh
@@ -2,7 +2,6 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 script_dir=$(cd "$(dirname "$0")" && pwd)
-set -o pipefail
 
 echo "Checking for azure CLI"
 which az
